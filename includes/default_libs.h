52a4a04b2097bea5ad79968046ac4f1b23ea356a
Angela Barrio Magdalena <abarrio-@c3r12s5.42madrid.com>
2023-12-22 12:44:33 +0100

    libreria de funciones c

---
+++ includes/default_libs.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   default_libs.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abarrio- <abarrio-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 10:25:31 by abarrio-          #+#    #+#             */
+/*   Updated: 2023/12/22 12:23:01 by abarrio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFAULT_LIBS_H
+# define DEFAULT_LIBS_H
+
+# include <fcntl.h>
+# include <limits.h>
+# include <math.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
